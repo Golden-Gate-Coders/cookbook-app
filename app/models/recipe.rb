c0af85662a577c6848f93e57e82ee1d9235ae050
Danish Mughal <mughal.danish@gmail.com>
2017-03-14 19:13:47 -0700

    Added some model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,16 @@
 class Recipe < ApplicationRecord
+
+  # @recipe.ingredients_split
+  def ingredients_split
+    ingredients.split(", ")
+  end
+
+  def directions_split
+    directions.split(", ")
+  end
+
+  def friendly_time
+    created_at.strftime("%B %e, %Y")
+  end
+
 end
