9c58caa4f06fcec75f1e91e82f9f9b970670e617
Danish Mughal <mughal.danish@gmail.com>
2017-03-15 20:04:08 -0700

    Added seed_dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
+Recipe.create!([
+  {title: "Mud Pie", ingredients: "Flour, water, mud.", prep_time: 84, directions: "Mix flour and water, add mud, bake at 350 degrees for 1 hour.", image: "http://cdn-image.myrecipes.com/sites/default/files/styles/300x300/public/image/recipes/sl/05/12/mocha-mud-pie-sl-257949-x.jpg?itok=_ChocANl", chef: "Mark Richardson"},
+  {title: "Pizza", ingredients: "Flour, water, pepperoni, cheese", prep_time: 455, directions: "Mix flour and water, add cheese and pepperoni, shape as a heart, bake in oven on self-clean mode.", image: "http://twopizzapals.weebly.com/uploads/2/3/6/0/23602608/1338890_orig.jpg", chef: "The Swedish Chef"},
+  {title: "Corn on the cob", ingredients: "Corn, cob", prep_time: 22, directions: "Place corn on the cob, grill for 20 minutes, enjoy!", image: "http://www.scientificamerican.com/sciam/cache/file/59C0122F-509C-4FEE-99A67FC20C50CEF1.jpg", chef: "Cornelius Vanderbilt"},
+  {title: "Licorice", ingredients: "Licorice", prep_time: 120, directions: "Buy licorice from store.", image: "http://cdn2.bigcommerce.com/server6200/aa739/product_images/uploaded_images/red-licorice.png", chef: "John Smith"},
+  {title: "Raw Eggs", ingredients: "Chicken", prep_time: 1, directions: "Squeeze the chicken", image: "http://swansonhealthcenter.com/wp-content/uploads/2011/07/Egg.jpg", chef: "John Smith"},
+  {title: "Empty Sandwich", ingredients: "Bread, Bread", prep_time: 2, directions: "Place slices of bread next to each other.", image: "http://www.phoenixisstrategic.com/wp-content/uploads/2013/05/blog-empty-sandwich.jpg", chef: "Jay Wengrow"}
+])
