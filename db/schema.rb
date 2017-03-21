553ca48112989cd0600f89829e0f3b082341d69c
Danish Mughal <mughal.danish@gmail.com>
2017-03-20 18:12:43 -0700

    Added chef_id to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321010247) do
+ActiveRecord::Schema.define(version: 20170321011057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170321010247) do
     t.datetime "updated_at",  null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "chef_id"
   end
 
 end
