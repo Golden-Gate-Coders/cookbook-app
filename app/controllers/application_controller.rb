c4305cd2b195d4e8d850ce5bc9b930b9b5ddc9ed
Danish Mughal <mughal.danish@gmail.com>
2017-03-21 21:02:12 -0700

    Added current_user helper method and navbar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+
+    # if session[:user_id]
+    #   @current_user = User.find_by(id: session[:user_id])
+    # else
+    #   @current_user = false
+    # end
+  end
+  helper_method :current_user
+
+
 end
